13150b47f6a33d6f57618890a240b3f64f8bd80d
Mario Moser <mario.moser@students.unibe.ch>
2019-05-17 18:34:38 +0200

    modified evaluation

--- evaluation.py
+++ evaluation.py
@@ -7,111 +7,86 @@ from tqdm import tqdm
 
 def main():
     ################################################
-    path = 'norm_results/*.txt'
-    files = glob.glob(path)
-    auc = []
-    auc_sum = []
-    score = []
-    zero_true_ids = False
 
-    recall_mean = []
+    file = np.genfromtxt('results/results_norm.txt', delimiter=',', dtype=str)
+    score = []
+
+    #reate an array, that has the same structure as gf
+    result_gf = np.zeros([ len(file[0])//2*len(file) , 2], object)
+
     precision_mean = []
 
-    # find true IDs
+    # file with key
+    gt = np.loadtxt('signaturedata/gt.txt', dtype=str)
 
-    transcr = np.loadtxt('ground-truth/transcription.txt', dtype=str)
-
-    # take only pages 300-304
-    transcr = transcr[2433:, :]
-
-    # norm_res: (8.5, 11.1, 0.1) ; res: (5, 25, 1) AND CHANGE > TO < for checking the threshold on line ~64
-    for i in tqdm(np.arange(8.5, 11.1, 0.1)):
-
-        recall_all = []
-        precision_all = []
-        for name in files:
-
-            # create array with sorted ids from result file
-            file = np.loadtxt(name, dtype=str)
-
-            ################################################
-
-            #find indeces in array with the same word in transcription: split the path into the keyword.
-            arr_index = np.where(transcr == name.split("\\")[-1].split(".")[0])
-
-            #create list with true IDs:
-            true_ids = []
-            for idx in arr_index[0]:
-                true_ids.append(transcr[idx,0])
-
-            #keywords which are not present in the test pages
-            if len(true_ids) == 0:
-                zero_true_ids = True
-            else:
-                zero_true_ids = False
-
-            #############################################
-            #find correct ids:
-
-            tp = 0
-            tn = 0
-            fp = 0
-
-            all_positive = len(true_ids)
+    threshold = []
+    precision_all = []
 
 
-            #create new array with only IDs over the threshold
-            file = file[np.where(np.array([float(x) for x in file[:,1]]) > i)] #todo: write >threshold
+    #go through different thresholds i
+    for i in tqdm(np.arange(0, 0.1, 0.001)):
 
-            #go through all result test IDs (from highest score to lowest)
-            for test_id in file[:,0]:
+        tp = 0
+        fp = 0
 
-                # calculate true positives:
-                if test_id in true_ids:
-                    tp += 1
-                    score.extend(list(file[np.where(file[:, 0] == test_id), 1][0]))
+
+        # user: genuine (g) and forgeries (f) signatures
+        # user1, signature_ID11, dissimilarity11, signature_ID12, dissimilarity12, ...
+        # example:
+        # 051, 46, 6.40341144, 21, 7.62949846, 17, 9.18516724, 03, 10.47132116, […]
+        for k, user in enumerate(file):
+
+            #go through every single signature: if score is below threshold (i) --> genuined(g); otherwise --> foreeriesed (f)
+            for e in range(len(user)//2):
+                #create first column:
+                row = k*(len(user)//2) + e
+                id_text = user[0] + "-" + user[e*2 + 1]
+                result_gf[row, 0] = id_text.replace(" ", "")
+
+                #create second column:
+                #if score is below threshold (i) --> genuined(g); otherwise --> foreeriesed (f)
+                if float(user[e*2+2]) < i:
+                    result_gf[row, 1] = "g"
                 else:
-                    fp += 1
+                    result_gf[row, 1] = "f"
 
-            if zero_true_ids == False:
-                recall = tp / all_positive
-                precision = tp / (tp + fp+0.0000000001)
 
-                recall_all.append(recall)
-                precision_all.append(precision)
+        result_gf = np.array(result_gf, dtype='str')
+        result_gf = result_gf[np.argsort(result_gf[:, 0])[::]]
 
-            #if there are zero words in the transcription file
+        for k1, line in enumerate(result_gf):
+            if line[1] == gt[k1,1]:
+                tp +=1
             else:
-                if tp == 0:
-                    recall, precision = 1, 1
-                else:
-                    recall, precision = 0, 0
+                fp +=1
+
+        precision = tp/ (tp + fp)
+
+        threshold.append(i)
+        precision_all.append(precision)
+
+        #precision_mean.append(np.mean(precision_all))
 
 
-        recall_mean.append(np.mean(recall_all))
-        precision_mean.append(np.mean(precision_all))
 
-
-
-
-    print(np.mean(recall_mean), np.mean(precision_mean))
-
-    auc = metrics.auc(recall_mean, precision_mean)
-    plt.plot(recall_mean, precision_mean)
-    plt.xlabel('recall')
+    # print(np.mean(recall_mean), np.mean(precision_mean))
+    #
+    # auc = metrics.auc(recall_mean, precision_mean)
+    plt.plot(threshold, precision_all)
+    plt.xlabel('threshold')
     plt.ylabel('precision')
-    plt.title(auc)
+    plt.title("Precision with different thresholds")
+    plt.ylim(0,1.2)
+
+    ymax = max(precision_all)
+    xpos = precision_all.index(ymax)
+    xmax = threshold[xpos]
+    plt.annotate('maximum precision: {} \n with threshold = {}'.format(ymax, xmax), xy=(xmax, ymax), xytext=(xmax, ymax + 0.1),
+                arrowprops=dict(facecolor='black', shrink=0.05),
+                )
+
+
     plt.show()
-
-
-    #calculation of tp mean score:
-    '''
-    intscore = []
-    for ele in score:
-        intscore.append(float(ele))
-    print(intscore)
-    print(np.mean(intscore))
-    '''
 
 
 if __name__ == '__main__':
